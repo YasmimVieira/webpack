b944a7d12ef6094bcb7f34333cd7250886e631f0
Yasmim Barbosa Vieira <yasmim.vieira@aluno.faculdadeimpacta.com.br>
2021-02-10 19:19:42 -0300

    chore: initial webpack configuration

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => {
+    return React.createElement('a',
+        { href: 'https://www.origamid.com' },
+        'Origamid',
+    );
+};
+
+export default App;
